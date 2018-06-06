2fbcccc9ef52b4d07bb47cc56b5911f1b7ac0c61
Thiago Spíndola <thiagorspindola@hotmail.com>
2018-06-06 00:55:24 -0300

    Save state

--- app/src/main/java/com/example/tspindola/notifyme/PackageInFragment.kt
+++ app/src/main/java/com/example/tspindola/notifyme/PackageInFragment.kt
@@ -5,13 +5,29 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
-class PackageInFragment : Fragment() {
+class PackageInFragment : Fragment(), View.OnClickListener {
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+            val v:View = inflater.inflate(R.layout.fragment_package_in, container, false)
+            val buttonAdd = v.findViewById<Button>(R.id.button_new_package_next)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.fragment_package_in, container, false)
+            buttonAdd?.setOnClickListener(this)
+
+            return v
+        }
 
     companion object {
         fun newInstance(): PackageInFragment = PackageInFragment()
+    }
+
+    override fun onClick(v:View?){
+        when(v?.id){
+            R.id.button_new_package_next -> addNewPackage()
+        }
+    }
+
+    fun addNewPackage(){
+        
     }
 }
